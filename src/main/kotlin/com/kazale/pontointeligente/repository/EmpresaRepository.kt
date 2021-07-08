70e6fbe8a0386f415d2a00e6798fc1fe9a006fd4
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2021-07-08 11:45:04 -0300

    12 -  finalizando repositories

---
+++ src/main/kotlin/com/kazale/pontointeligente/repository/EmpresaRepository.kt
@@ -0,0 +1,9 @@
+package com.kazale.pontointeligente.repository
+
+import com.kazale.pontointeligente.documents.Empresa
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface EmpresaRepository : MongoRepository<Empresa, String> {
+
+    fun findByCnpj(cnpj: String): Empresa?
+}
---
+++ src/main/kotlin/com/kazale/pontointeligente/repository/FuncionarioRepository.kt
@@ -0,0 +1,11 @@
+package com.kazale.pontointeligente.repository
+
+import com.kazale.pontointeligente.documents.Funcionario
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface FuncionarioRepository : MongoRepository<Funcionario, String> {
+
+    fun findByEmail(email: String): Funcionario?
+
+    fun findByCpf(cpf: String): Funcionario?
+}
