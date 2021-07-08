7dc1f79a4da838563c52c056b73feba95cbc2d38
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2021-07-07 21:30:15 -0300

    10 - Finalizando docs mongo

--- src/main/kotlin/com/kazale/pontointeligente/documents/Lancamento.kt
+++ src/main/kotlin/com/kazale/pontointeligente/documents/Lancamento.kt
@@ -1,4 +1,16 @@
 package com.kazale.pontointeligente.documents
 
-class Lancamento {
-}
+import com.kazale.pontointeligente.enums.TipoEnum
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.Date
+
+@Document
+data class Lancamento(
+    val data: Date,
+    val tipo: TipoEnum,
+    val funcionarioId: String,
+    val descricao: String? = "",
+    val localizacao: String? = "",
+    @Id val id: String? = null
+)
